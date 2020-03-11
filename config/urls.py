3c416909cde02594e38fbde3b0ef12d013dda1e0
zenjutahi <zenjutahi@gmail.com>
2020-03-11 13:07:42 +0300

    [chore] - add models to project

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    re_path(r'^', include('shop.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/logout', TemplateView.as_view(template_name='logout.html'), name='logout'),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL,
+						document_root=settings.MEDIA_ROOT)
