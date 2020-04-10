11c6d6a7504b8d94dd8ecad8de3d37334e0cd257
zenjutahi <zenjutahi@gmail.com>
2020-04-10 04:12:51 +0300

    [feature] - restructure zenj manage intance to barcode

--- config/settings.py
+++ config/settings.py
@@ -130,11 +130,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
