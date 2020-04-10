182e745b8149780b884fd7d87c1c483155090fa9
zenjutahi <zenjutahi@gmail.com>
2020-04-10 04:13:01 +0300

    [feature] - restructure zenj manage intance to barcode

--- config/urls.py
+++ config/urls.py
@@ -14,19 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    re_path(r'^', include('shop.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('admin/', admin.site.urls),
     path('accounts/logout', TemplateView.as_view(template_name='logout.html'), name='logout'),
+    path('admin/', admin.site.urls),
+    re_path(r'^', include('shop.urls')),
 ]
-
-if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL,
-						document_root=settings.MEDIA_ROOT)
